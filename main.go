f5bcc9b4b46b6d4b3e43dd2a42128172818cea02
Martin Polden <martin.polden@gmail.com>
2017-05-12 20:56:15 +0200

    Add CLI

---
+++ main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"log"
+	"net"
+	"net/http"
+	"os"
+
+	flags "github.com/jessevdk/go-flags"
+	"github.com/mpolden/wakeonlan/api"
+)
+
+func main() {
+	var opts struct {
+		CacheFile string `short:"c" long:"cache" description:"Path to cache file" required:"true" value-name:"FILE"`
+		SourceIP  net.IP `short:"b" long:"bind" description:"IP address to bind to when sending WOL packets" value-name:"IP"`
+		Listen    string `short:"l" long:"listen" description:"Listen address" value-name:"ADDR" default:":8080"`
+		StaticDir string `short:"s" long:"static" description:"Path to directory containing static assets" value-name:"DIR"`
+	}
+	_, err := flags.ParseArgs(&opts, os.Args)
+	if err != nil {
+		os.Exit(1)
+	}
+
+	api := api.New(opts.CacheFile)
+	api.StaticDir = opts.StaticDir
+	api.SourceIP = opts.SourceIP
+	log.Printf("Listening on %s", opts.Listen)
+	if err := http.ListenAndServe(opts.Listen, api.Handler()); err != nil {
+		log.Fatal(err)
+	}
+}
